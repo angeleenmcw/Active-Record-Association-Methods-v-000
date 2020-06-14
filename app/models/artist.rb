2fd09909b380ad67a9ad3b15db38aacb7db5c857
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-06-14 04:57:24 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,14 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    Show.where("genre = ?", show.first("songs"))
+    Show.where("genre = ?", Show.first("songs"))
   end
 
   def song_count
-    Show.sum("songs")
+    Show.sum("songs", Show.artist)
   end
 
   def genre_count
-    Show.sum("genre")
+    Show.sum("genre", Show.artist)
   end
 end
